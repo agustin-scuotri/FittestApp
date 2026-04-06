456ec1af50afac114204981cd22b956b5cc997de
agustin-scuotri <induccion3projectsconsulting@gmail.com>
2026-04-06 17:15:03 -0300

    Configure PostgreSQL, rename main class to FittestAppApplication

---
+++ fittestapp-api/src/main/java/com/fittestapp/FittestAppApplication.java
@@ -0,0 +1,13 @@
+package com.fittestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FittestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FittestAppApplication.class, args);
+	}
+
+}
